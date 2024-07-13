5b6d8f30d36bb55baf0fdeb3c334da80dd563440
Jonatas Macedo <jonatasmacedo70@hotmail.com>
2024-07-13 15:17:47 -0300

    fix: add request to get the current value

--- extract_values.py
+++ extract_values.py
@@ -1,5 +1,7 @@
 import time
 import requests
+import json
+import urllib.parse
 
 class extract_values:
     def __init__(self, steam_user):
@@ -7,10 +9,30 @@ class extract_values:
         self.steam_user = steam_user
 
     def get_inventory(self):
-        request_url = self.steam_url + "inventory/76561198286681262/730/2?l=brazilian&count=75"
+        request_url = self.steam_url + "inventory/" + self.steam_user + "/730/2?l=brazilian&count=100"
+        response = requests.get(request_url)
+        response = json.loads(response.content)
+        return response['descriptions']
 
-        return requests.get(url);
+    def get_value(self, market_hash_name):
+        time.sleep(5)
+        market_hash_name = urllib.parse.quote_plus(market_hash_name)
+        request_url = self.steam_url + "market/priceoverview/?appid=730&currency=7&market_hash_name=" + market_hash_name
+
+        response = requests.get(request_url)
+        response = json.loads(response.content)
+        return response.get('lowest_price', 'R$ 0.00')
 
     def create_file(self):
         microtime = str(round(time.time() * 1000))
         file = open(microtime+'.csv', "w", encoding="utf8")
+
+    def process(self, app):
+        inventory = self.get_inventory()
+        current_values = {}
+        for iten in inventory :
+            if (not 'Graffiti' in iten['market_hash_name']) and (iten['marketable'] == 1):
+                app.setTitle('current', iten['market_hash_name'])
+                current_values[iten['market_hash_name']] = self.get_value(iten['market_hash_name'])
+
+        return current_values
--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from appJar import gui
 from process_data import process_data
+from extract_values import extract_values
 import json
+import csv
 
 def press():
     marketHistory = app.getEntry("marketHistory")
     idUser = app.getEntry("idUser")
     file = open(marketHistory, "r", encoding="utf8")
     file = file.read()
-    pd = process_data(file)
-    result = pd.process()
-    resultJson = open('result.json', 'x', encoding="utf8")
-    resultJson.write(json.dumps(result))
-    # print("abc".split('b').pop())
+    pd = process_data(file).process()
+    vd = extract_values(idUser).process(app)
+
+    with open('result.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=';')
+        spamwriter.writerow(['Item', 'Valor Pago', 'Valor Atual', 'Diferença'])
+        index = 2
+        for key, value in vd:
+            spamwriter.writerow([key, pd[key], value, "=((C{index}-B{index})/B{index})"])
+            index = index + 1
+
+        spamwriter.writerow([])
+        spamwriter.writerow(['Total', '=SUM(B2:B39)', '=(SUM(C2:C39))', '=((C41-B41)/B41)'])
 
 app = gui("CS Inventory Dumper", '500x200')
 # app.addLabel("lblMarketHistory", "Market history:")
@@ -21,4 +31,5 @@ app.addLabel('guide2', 'Input the saved file')
 app.addLabelFileEntry(title="marketHistory", label="Market history:")
 app.addLabelEntry(title='idUser', label="ID User:")
 app.addButton('dump', press)
+app.addLabel('current', '')
 app.go()
